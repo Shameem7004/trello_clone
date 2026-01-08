12af2242fc63619ee62254bf7d6f2436049b2da2
Shameem7004 <shameem.alam.7004@gmail.com>
2026-01-08 17:04:34 +0530

    .env changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true
 }));
 app.use(express.json());
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,4 +4,8 @@ const API = axios.create({
   baseURL: "http://localhost:3000",
 });
 
+export const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:4000";
+
+
 export default API;
